77d8a8089d9595280a4ed938a3f384517af695e7
bram.deraeve <bram.deraeve@student.howest.be>
2022-09-25 12:14:54 +0200

    closeable window

--- src/main.c
+++ src/main.c
@@ -1,27 +1,52 @@
 #include "SDL.h"
 
+#define FPS 60
+
 int main(int argc, char *argv[])
 {
-  SDL_Init(SDL_INIT_VIDEO);
+    SDL_Init(SDL_INIT_VIDEO);
 
-  SDL_Window *window = SDL_CreateWindow(
-    "SDL2Test",
-    SDL_WINDOWPOS_UNDEFINED,
-    SDL_WINDOWPOS_UNDEFINED,
-    640,
-    480,
-    0
-  );
+    char appIsRunning = 1;
 
-  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
-  SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-  SDL_RenderClear(renderer);
-  SDL_RenderPresent(renderer);
+    SDL_Window *window = SDL_CreateWindow(
+        "SDL2Test",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        640,
+        480,
+        0);
 
-  SDL_Delay(3000);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
 
-  SDL_DestroyWindow(window);
-  SDL_Quit();
+    SDL_Event e;
+    while (appIsRunning == 1)
+    {
+        while (SDL_PollEvent(&e))
+        {
+            switch (e.type)
+            {
+            case SDL_QUIT:
+                appIsRunning = 0;
+                break;
+            default:
+                break;
+            }
+        }
 
-  return 0;
+        float deltaTime = 1.0;
+        // update(deltaTime);
+
+        // render();
+
+        // If frames were 30, wait 33 ms before running the loop again
+        SDL_Delay(1000 / FPS);
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
 }
